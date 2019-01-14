2b5cf5f204c1daf6707a86e4e487af47dd8a2553
Dialest <dialest@hotmail.com>
2019-01-13 19:59:22 -0800

    Set up window creation tool. Wrote balance.html to find values for game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,3 +33,78 @@ function Warrior(name, hp, atk, cntr) {
 
 // enemy.counter(player);
 
+function boxCreate(container) {
+    $("#" + container).css("position", "relative");
+    $("#" + container).html(" \
+    <div class='tlc'></div> \
+    <div class='tb'></div> \
+    <div class='trc'></div> \
+    <div class='lb'></div> \
+    <div class='rb'></div> \
+    <div id=" + container + "-content> \
+    </div> \
+    <div class='blc'></div> \
+    <div class='bb'></div> \
+    <div class='brc'></div> \
+    ");
+}
+
+
+// boxtop("box");
+// $("#box-content").html("<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam laboriosam repudiandae aut doloremque alias molestiae, odit sint pariatur atque porro autem dolorum officia, dignissimos delectus unde reiciendis aliquam facilis quam!</p>");
+
+// setTimeout(function(){ $("#box").remove() }, 3000);
+
+// $("section").append("<div id='container2' class='container'></div>");
+// boxtop("container2");
+
+// create opening page
+// create char select screen
+// BALANCE CHARACTERS
+// Create enemy select screen
+// create battle scene
+// define attack function
+
+// opening > char select > enemy select > battle > attack(repeat) > Loss or enemy select > battle
+
+
+
+$(document).bind('keyup', function(){
+    console.log("KEY");
+    $("#gamescreen").append("<p> HELLO </p>");
+    $(document).unbind('keyup');
+    $("#gamescreen").css("background", "black");
+    $("#gamescreen").html(" \
+    <div class='row' \
+        <div class='col-md-12' id='instructions'> \
+        </div> \
+    </div> \
+    <div class='row'> \
+        <div class='col-md-6 charSelect' id='fighter'> \
+        </div> \
+        <div class='col-md-6 charSelect' id='thief'> \
+        </div> \
+    </div> \
+    <div class='row'> \
+        <div class='col-md-6 charSelect' id='blackBelt'> \
+        </div> \
+        <div class='col-md-6 charSelect' id='blackMage'> \
+        </div> \
+    </div> \
+    ");
+    
+    boxCreate("instructions");
+    $("#instructions-content").html(" \
+    <p>This is where instructions go.</p> \
+    <p>You'll pick someone.</p> \
+    <p>Pick someone below.</p> \
+    ")
+    boxCreate("fighter");
+    $("#fighter-content").html("<p>Fighter</p>");
+    boxCreate("thief");
+    $("#thief-content").html("<p>Thief</p>");
+    boxCreate("blackBelt");
+    $("#blackBelt-content").html("<p>Black Belt</p>");
+    boxCreate("blackMage");
+    $("#blackMage-content").html("<p>Black Mage</p>");
+});
